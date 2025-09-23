70f7443ef51b202e72df3561dbdd944de977c019
Jinwoo Sung <jwsung91@gmail.com>
2025-09-23 22:44:51 +0900

    chore: rename param for ul

--- examples/echo/echo_tcp_server.cc
+++ examples/echo/echo_tcp_server.cc
@@ -8,24 +8,24 @@ int main(int argc, char** argv) {
       (argc > 1) ? static_cast<unsigned short>(std::stoi(argv[1])) : 9000;
 
   unilink::TcpServerConfig cfg{port};
-  auto ch = unilink::create(cfg);
-  ch->on_state([&](unilink::LinkState s) {
+  auto ul = unilink::create(cfg);
+  ul->on_state([&](unilink::LinkState s) {
     auto state_msg = "state=" + std::string(unilink::to_cstr(s));
     unilink::log_message("[server]", "STATE", state_msg);
   });
 
-  ch->on_bytes([&](const uint8_t* p, size_t n) {
+  ul->on_bytes([&](const uint8_t* p, size_t n) {
     std::string s(reinterpret_cast<const char*>(p), n);
     unilink::log_message("[server]", "RX", s);
     unilink::log_message("[server]", "TX", s);
-    ch->async_write_copy(p, n);
+    ul->async_write_copy(p, n);
   });
 
-  ch->start();
+  ul->start();
 
   // 프로그램이 Ctrl+C로 종료될 때까지 무한정 대기합니다.
   std::promise<void>().get_future().wait();
 
-  ch->stop();
+  ul->stop();
   return 0;
 }
