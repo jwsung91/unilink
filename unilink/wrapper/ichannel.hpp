6334bfe8b543bfdf770aed7c82d386553bef077c
Jinwoo Sung <jwsung91@gmail.com>
2025-10-09 18:36:08 +0900

    docs: add comments to main api

--- unilink/wrapper/ichannel.hpp
+++ unilink/wrapper/ichannel.hpp
@@ -7,33 +7,33 @@
 namespace unilink {
 namespace wrapper {
 
-// 모든 Wrapper 통신 클래스의 공통 인터페이스
+// Common interface for all Wrapper communication classes
 class ChannelInterface {
-public:
-    using DataHandler = std::function<void(const std::string&)>;
-    using ConnectHandler = std::function<void()>;
-    using DisconnectHandler = std::function<void()>;
-    using ErrorHandler = std::function<void(const std::string&)>;
+ public:
+  using DataHandler = std::function<void(const std::string&)>;
+  using ConnectHandler = std::function<void()>;
+  using DisconnectHandler = std::function<void()>;
+  using ErrorHandler = std::function<void(const std::string&)>;
 
-    virtual ~ChannelInterface() = default;
-    
-    // 공통 메서드들
-    virtual void start() = 0;
-    virtual void stop() = 0;
-    virtual void send(const std::string& data) = 0;
-    virtual void send_line(const std::string& line) = 0;
-    virtual bool is_connected() const = 0;
-    
-    // 이벤트 핸들러 설정
-    virtual ChannelInterface& on_data(DataHandler handler) = 0;
-    virtual ChannelInterface& on_connect(ConnectHandler handler) = 0;
-    virtual ChannelInterface& on_disconnect(DisconnectHandler handler) = 0;
-    virtual ChannelInterface& on_error(ErrorHandler handler) = 0;
-    
-    // 편의 메서드들
-    virtual ChannelInterface& auto_start(bool start = true) = 0;
-    virtual ChannelInterface& auto_manage(bool manage = true) = 0;
+  virtual ~ChannelInterface() = default;
+
+  // Common methods
+  virtual void start() = 0;
+  virtual void stop() = 0;
+  virtual void send(const std::string& data) = 0;
+  virtual void send_line(const std::string& line) = 0;
+  virtual bool is_connected() const = 0;
+
+  // Event handler setup
+  virtual ChannelInterface& on_data(DataHandler handler) = 0;
+  virtual ChannelInterface& on_connect(ConnectHandler handler) = 0;
+  virtual ChannelInterface& on_disconnect(DisconnectHandler handler) = 0;
+  virtual ChannelInterface& on_error(ErrorHandler handler) = 0;
+
+  // Convenience methods
+  virtual ChannelInterface& auto_start(bool start = true) = 0;
+  virtual ChannelInterface& auto_manage(bool manage = true) = 0;
 };
 
-} // namespace wrapper
-} // namespace unilink
+}  // namespace wrapper
+}  // namespace unilink
