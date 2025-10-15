e37974cee8ff5dd770197852fdb502c3442dae28
Jinwoo Sung <jwsung91@gmail.com>
2025-10-15 23:13:25 +0900

    fix: winsock

--- unilink/common/platform.hpp
+++ unilink/common/platform.hpp
@@ -25,8 +25,11 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <windows.h>
-#endif
+#include <threadpoolapiset.h>
+#endif  // defined(_WIN32)
 
 namespace unilink {
 namespace common {
