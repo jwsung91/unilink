a807c6abd12551de49649c5c9452932e1ec15d69
Jinwoo Sung <jwsung91@gmail.com>
2025-11-27 02:52:18 +0900

    fix(cmake): Correctly handle build flags for different generator types
    Reverted the change that used generator expressions for single-configuration generators, as it was causing build failures. The correct approach is to use 'if(CMAKE_BUILD_TYPE ...)' for single-config generators and generator expressions for multi-config generators. This commit fixes the build error on Linux while keeping the correct configuration for Windows.

--- cmake/UnilinkCompiler.cmake
+++ cmake/UnilinkCompiler.cmake
@@ -40,125 +40,246 @@ if(MSVC)
     endif()
   endif()
   
-  # MSVC-specific optimizations and debug information
-  add_compile_options(
-    $<$<CONFIG:Release>:/O2 /Ob2 /Oi /Ot /Oy /GL>
-    $<$<CONFIG:Debug>:/Zi>
-    $<$<CONFIG:RelWithDebInfo>:/O2 /Zi>
-    $<$<CONFIG:MinSizeRel>:/O1 /Os>
-  )
-  add_link_options($<$<CONFIG:Release>:/LTCG>)
+    # MSVC-specific optimizations and debug information
   
-elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-  # GCC or Clang
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(UNILINK_COMPILER_GCC ON)
-  else()
-    set(UNILINK_COMPILER_CLANG ON)
-  endif()
+    add_compile_options(
   
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "9.0")
-    message(FATAL_ERROR "GCC 9.0+ or Clang 10.0+ required for C++17 support")
-  endif()
-
-  if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
-    # Ubuntu version detection for compiler-specific settings
-    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11.0")
-      set(UNILINK_UBUNTU_20_04 ON)
-      add_definitions(-DUNILINK_UBUNTU_20_04=1)
-      message(STATUS "Detected Ubuntu 20.04 compatibility mode")
-    elseif(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "13.0")
-      set(UNILINK_UBUNTU_22_04 ON)
-      add_definitions(-DUNILINK_UBUNTU_22_04=1)
-      message(STATUS "Detected Ubuntu 22.04 compatibility mode")
+      $<$<CONFIG:Release>:/O2 /Ob2 /Oi /Ot /Oy /GL>
+  
+      $<$<CONFIG:Debug>:/Zi>
+  
+      $<$<CONFIG:RelWithDebInfo>:/O2 /Zi>
+  
+      $<$<CONFIG:MinSizeRel>:/O1 /Os>
+  
+    )
+  
+    add_link_options($<$<CONFIG:Release>:/LTCG>)
+  
+    
+  
+  elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
+  
+    # GCC or Clang
+  
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  
+      set(UNILINK_COMPILER_GCC ON)
+  
     else()
-      set(UNILINK_UBUNTU_24_04 ON)
-      add_definitions(-DUNILINK_UBUNTU_24_04=1)
-      message(STATUS "Detected Ubuntu 24.04+ compatibility mode")
+  
+      set(UNILINK_COMPILER_CLANG ON)
+  
     endif()
-  elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
-    add_definitions(-DUNILINK_PLATFORM_MACOS=1)
-    message(STATUS "Detected macOS compatibility mode")
+  
+    
+  
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "9.0")
+  
+      message(FATAL_ERROR "GCC 9.0+ or Clang 10.0+ required for C++17 support")
+  
+    endif()
+  
+  
+  
+    if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  
+      # Ubuntu version detection for compiler-specific settings
+  
+      if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "11.0")
+  
+        set(UNILINK_UBUNTU_20_04 ON)
+  
+        add_definitions(-DUNILINK_UBUNTU_20_04=1)
+  
+        message(STATUS "Detected Ubuntu 20.04 compatibility mode")
+  
+      elseif(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "13.0")
+  
+        set(UNILINK_UBUNTU_22_04 ON)
+  
+        add_definitions(-DUNILINK_UBUNTU_22_04=1)
+  
+        message(STATUS "Detected Ubuntu 22.04 compatibility mode")
+  
+      else()
+  
+        set(UNILINK_UBUNTU_24_04 ON)
+  
+        add_definitions(-DUNILINK_UBUNTU_24_04=1)
+  
+        message(STATUS "Detected Ubuntu 24.04+ compatibility mode")
+  
+      endif()
+  
+    elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+  
+      add_definitions(-DUNILINK_PLATFORM_MACOS=1)
+  
+      message(STATUS "Detected macOS compatibility mode")
+  
+    else()
+  
+      add_definitions(-DUNILINK_PLATFORM_POSIX=1)
+  
+      message(STATUS "Detected generic POSIX compatibility mode")
+  
+    endif()
+  
+    
+  
+    # Common flags for GCC and Clang
+  
+    if(UNILINK_ENABLE_WARNINGS)
+  
+      add_compile_options(-Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion)
+  
+      
+  
+      # Ubuntu 20.04 specific warning suppressions
+  
+      if(UNILINK_UBUNTU_20_04)
+  
+        add_compile_options(-Wno-deprecated-declarations)
+  
+        add_compile_options(-Wno-unused-variable)
+  
+        message(STATUS "Applied Ubuntu 20.04 warning suppressions")
+  
+      endif()
+  
+      
+  
+      # Additional warnings for Clang
+  
+      if(UNILINK_COMPILER_CLANG)
+  
+        add_compile_options(-Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic)
+  
+      endif()
+  
+      
+  
+      if(UNILINK_ENABLE_WERROR)
+  
+        add_compile_options(-Werror)
+  
+      endif()
+  
+    endif()
+  
+    
+  
+    # Visibility settings (disabled for now to avoid linking issues)
+  
+    # add_compile_options(-fvisibility=hidden -fvisibility-inlines-hidden)
+  
+    
+  
+    # Optimization flags
+  
+    if(CMAKE_BUILD_TYPE STREQUAL "Release")
+  
+      add_compile_options(-O3 -DNDEBUG)
+  
+      if(UNILINK_ENABLE_LTO)
+  
+        add_compile_options(-flto)
+  
+        add_link_options(-flto)
+  
+      endif()
+  
+    elseif(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  
+      add_compile_options(-g -O0)
+  
+    endif()
+  
+    
+  
+    # Sanitizer support
+  
+    if(UNILINK_ENABLE_SANITIZERS AND CMAKE_BUILD_TYPE STREQUAL "Debug")
+  
+      if(UNILINK_ENABLE_ASAN)
+  
+        add_compile_options(-fsanitize=address)
+  
+        add_link_options(-fsanitize=address)
+  
+      endif()
+  
+      if(UNILINK_ENABLE_UBSAN)
+  
+        add_compile_options(-fsanitize=undefined)
+  
+        add_link_options(-fsanitize=undefined)
+  
+      endif()
+  
+      if(UNILINK_ENABLE_TSAN)
+  
+        add_compile_options(-fsanitize=thread)
+  
+        add_link_options(-fsanitize=thread)
+  
+      endif()
+  
+    endif()
+  
+    
+  
+    # Coverage support
+  
+    if(UNILINK_ENABLE_COVERAGE)
+  
+      add_compile_options(--coverage)
+  
+      add_link_options(--coverage)
+  
+    endif()
+  
+    
+  
   else()
-    add_definitions(-DUNILINK_PLATFORM_POSIX=1)
-    message(STATUS "Detected generic POSIX compatibility mode")
+  
+    message(WARNING "Unknown compiler: ${CMAKE_CXX_COMPILER_ID}")
+  
   endif()
   
-  # Common flags for GCC and Clang
-  if(UNILINK_ENABLE_WARNINGS)
-    add_compile_options(-Wall -Wextra -Wpedantic -Wconversion -Wsign-conversion)
-    
-    # Ubuntu 20.04 specific warning suppressions
-    if(UNILINK_UBUNTU_20_04)
-      add_compile_options(-Wno-deprecated-declarations)
-      add_compile_options(-Wno-unused-variable)
-      message(STATUS "Applied Ubuntu 20.04 warning suppressions")
+  
+  
+  # Set C++ standard (will be set later when target is created)
+  
+  
+  
+  # Platform-specific settings
+  
+  if(WIN32)
+  
+    add_definitions(-DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_WIN32_WINNT=0x0A00)
+  
+    if(UNILINK_BUILD_SHARED)
+  
+      add_definitions(-DUNILINK_EXPORTS)
+  
     endif()
-    
-    # Additional warnings for Clang
-    if(UNILINK_COMPILER_CLANG)
-      add_compile_options(-Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic)
+  
+  elseif(UNIX)
+  
+    if(APPLE)
+  
+      # Enable full BSD/POSIX feature set on macOS (needed for networking macros like NI_MAXHOST)
+  
+      add_definitions(-D_DARWIN_C_SOURCE)
+  
+    else()
+  
+      add_definitions(-D_POSIX_C_SOURCE=200809L)
+  
     endif()
-    
-    if(UNILINK_ENABLE_WERROR)
-      add_compile_options(-Werror)
-    endif()
+  
   endif()
   
-  # Visibility settings (disabled for now to avoid linking issues)
-  # add_compile_options(-fvisibility=hidden -fvisibility-inlines-hidden)
   
-  # Optimization and debug flags
-  add_compile_options(
-    $<$<CONFIG:Release>:-O3 -DNDEBUG>
-    $<$<CONFIG:Debug>:-g -O0>
-    $<$<CONFIG:RelWithDebInfo>:-O2 -g -DNDEBUG>
-    $<$<CONFIG:MinSizeRel>:-Os -DNDEBUG>
-  )
-  if(UNILINK_ENABLE_LTO)
-    add_compile_options($<$<CONFIG:Release>:-flto>)
-    add_link_options($<$<CONFIG:Release>:-flto>)
-  endif()
-  
-  # Sanitizer support
-  if(UNILINK_ENABLE_SANITIZERS)
-    if(UNILINK_ENABLE_ASAN)
-      add_compile_options($<$<CONFIG:Debug>:-fsanitize=address>)
-      add_link_options($<$<CONFIG:Debug>:-fsanitize=address>)
-    endif()
-    if(UNILINK_ENABLE_UBSAN)
-      add_compile_options($<$<CONFIG:Debug>:-fsanitize=undefined>)
-      add_link_options($<$<CONFIG:Debug>:-fsanitize=undefined>)
-    endif()
-    if(UNILINK_ENABLE_TSAN)
-      add_compile_options($<$<CONFIG:Debug>:-fsanitize=thread>)
-      add_link_options($<$<CONFIG:Debug>:-fsanitize=thread>)
-    endif()
-  endif()
-  
-  # Coverage support
-  if(UNILINK_ENABLE_COVERAGE)
-    add_compile_options(--coverage)
-    add_link_options(--coverage)
-  endif()
-  
-else()
-  message(WARNING "Unknown compiler: ${CMAKE_CXX_COMPILER_ID}")
-endif()
-
-# Set C++ standard (will be set later when target is created)
-
-# Platform-specific settings
-if(WIN32)
-  add_definitions(-DWIN32_LEAN_AND_MEAN -DNOMINMAX -D_WIN32_WINNT=0x0A00)
-  if(UNILINK_BUILD_SHARED)
-    add_definitions(-DUNILINK_EXPORTS)
-  endif()
-elseif(UNIX)
-  if(APPLE)
-    # Enable full BSD/POSIX feature set on macOS (needed for networking macros like NI_MAXHOST)
-    add_definitions(-D_DARWIN_C_SOURCE)
-  else()
-    add_definitions(-D_POSIX_C_SOURCE=200809L)
-  endif()
-endif()
