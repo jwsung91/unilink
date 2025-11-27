bb214d7400b52ba272b4c50fa4007aca6886eb24
Jinwoo Sung <jwsung91@gmail.com>
2025-11-27 23:18:51 +0900

    fix(test): resolve test failures on macOS and Windows
    Fixes a SEGFAULT in tcp_server tests on macOS by adding thread-safety to the TcpServer wrapper. Also fixes Windows build failures by using absolute paths in CMakeLists.txt for test files.

--- unilink/wrapper/tcp_server/tcp_server.cc
+++ unilink/wrapper/tcp_server/tcp_server.cc
@@ -81,7 +81,7 @@ void TcpServer::stop() {
   if (local_channel) {
     local_channel->stop();
     // Allow async operations to complete
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 }
 
